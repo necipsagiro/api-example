6b350a69651a0adfdd426aab06bf1129c61d8713
Necip <sagiroglun22@itu.edu.tr>
2023-04-20 16:00:36 +0300

    results done

--- main.py
+++ main.py
@@ -19,6 +19,27 @@ temps = [
         location="Germany"
     ),
     Temperature(
+        timestamp="1681895166",
+        timezone="UTC",
+        measurement=4,
+        unit="F",
+        location="Germany"
+    ),
+    Temperature(
+        timestamp="1681895166",
+        timezone="UTC",
+        measurement=8,
+        unit="F",
+        location="Germany"
+    ),
+    Temperature(
+        timestamp="1681895166",
+        timezone="UTC",
+        measurement=10,
+        unit="F",
+        location="Germany"
+    ),
+    Temperature(
         timestamp="1681895168",
         timezone="UTC",
         measurement=2,
@@ -26,6 +47,15 @@ temps = [
         location="France"
     ),
 ]
+
+
+def median(arr):
+    arr = sorted(arr)
+    if len(arr) % 2:
+        return arr[len(arr) // 2]
+    else:
+        return (arr[len(arr) // 2] + arr[len(arr) // 2 - 1]) / 2
+
 
 app = FastAPI()
 
@@ -51,5 +81,10 @@ def get_stats(since: str, until: str, location: str, unit: str):
     result = []
     for temp in temps:
         if temp.location == location and temp.unit == unit and temp.timestamp >= since and temp.timestamp <= until:
-            result.append(temp)
-    return
+            result.append(temp.measurement)
+
+    return {
+        "average": sum(result) / len(result),
+        "median": median(result),
+        "count": len(result),
+    }
